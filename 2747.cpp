f6a038c7c704978ae5423f9faa26f1cd28649d4f
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2018-07-17 01:02:28 +0600

    Create 2747.cpp

---
+++ 2747.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+int main(){
+// Using brain
+//    cout << endl;
+//    for(int i=0; i<39; i++){
+//        cout << "-";
+//    }
+//    cout << endl;
+//    cout << endl;
+//    for(int j=0; j<5;j++){
+//        cout << "|";
+//        for(int k=0; k<37; k++){
+//            cout << " ";
+//        }
+//        cout <<"|" << endl;
+//        cout << endl;
+//    }
+//    for(int i=0; i<39; i++){
+//        cout << "-";
+//    }
+//    cout << endl;
+
+//using hand
+
+    printf("---------------------------------------\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("---------------------------------------\n");
+    return 0;
+}
