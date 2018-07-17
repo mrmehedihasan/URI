6380ad64f92581a5ac4fb5a56f0827e6ae5a0e67
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2018-07-17 20:44:10 +0600

    Create 1197.cpp

---
+++ 1197.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int a,v;
+    while(scanf("%d %d", &a,&v) !=EOF){
+        cout << 2*a*v << endl;
+    }
+    return 0;
+}
