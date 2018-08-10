843b46898a24a2bdc710b50dc7f557e5e9bbcad2
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2018-08-10 17:24:11 +0600

    Create 1067.cpp

---
+++ 1067.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin>>t;
+    
+    for(int i=1; i<=t; i++){
+        if(i%2==1){
+            cout << i << endl;
+        }
+    }
+ 
+    return 0;
+}
